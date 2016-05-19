ece0b902c8a8325456f0bd9035406450d82e83c3
ddhunter13 <1310nero@mail.ru>
2016-05-19 19:31:35 +0300

    mainGameActivityWorks

--- app/src/main/java/myprojects/reversi/HumanPlayer.java
+++ app/src/main/java/myprojects/reversi/HumanPlayer.java
@@ -1,5 +1,9 @@
 package myprojects.reversi;
 
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
 import java.util.Scanner;
 
 /**
@@ -8,9 +12,13 @@ import java.util.Scanner;
 public class HumanPlayer implements Player{
 
     private int color;
+    Thread gameThread;
+    GridView gameGrid;
 
-    HumanPlayer(int col){
+    HumanPlayer(int col, Thread thread, GridView grid){
         this.color = col;
+        this.gameGrid = grid;
+        this.gameThread = thread;
     }
 
     @Override
@@ -22,19 +30,36 @@ public class HumanPlayer implements Player{
     public boolean makeMove(Field field){
         int[] res = new int[2];
         res = this.moveAsk(field);
-        while(FieldAnalyzer.analyze(field, res[0] - 1, res[1] - 1, color) == 0){
+        while(FieldAnalyzer.analyze(field, res[0], res[1], color) == 0){
             res = this.moveAsk(field);
         }
-        field.setCellState(res[0] - 1, res[1] - 1, color);
+        field.setCellState(res[0], res[1], color);
         return true;
     }
 
     @Override
     public int[] moveAsk(Field field){
-        Scanner in = new Scanner(System.in);
-        int[] res = new int[2];
-        res[0] = in.nextInt();
-        res[1] = in.nextInt();
+        final int[] res = new int[2];
+
+        gameGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                res[1] = (int) (position / 8);
+                res[0] = position % 8;
+                synchronized (gameThread) {
+                    gameThread.notify();
+                }
+            }
+        });
+        synchronized (gameThread) {
+            try {
+                gameThread.wait();
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+        gameGrid.setOnItemClickListener(null);
+
         return res;
     }
 
--- app/src/main/java/myprojects/reversi/MainActivity.java
+++ app/src/main/java/myprojects/reversi/MainActivity.java
@@ -15,23 +15,17 @@ public class MainActivity extends Activity {
     private Player player1;
     private Player player2;
     Thread gameThread;
-    int xMovePos, yMovePos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        xMovePos = -1;
-        yMovePos = -1;
-
         fieldGrid = (GridView) findViewById(R.id.field);
         fieldGrid.setNumColumns(8);
         fieldGrid.setEnabled(true);
 
         field = new Field(System.out);
-        player2 = new HumanPlayer(1);
-        player1 = new AIPlayer(2);
 
         revAdapter = new GridAdapter(this, 8, 8, this.field);
         fieldGrid.setAdapter(revAdapter);
@@ -41,93 +35,40 @@ public class MainActivity extends Activity {
                 gameProcess();
             }
         });
+
+        player1 = new HumanPlayer(1, gameThread, fieldGrid);
+        player2 = new AIPlayer(2);
+
         gameThread.start();
-    }
-
-    private void setMovePos(int i, int j) {
-        xMovePos = i;
-        yMovePos = j;
-    }
-
-    private int[] getMovePos() {
-        int[] result = new int[2];
-        result[0] = xMovePos;
-        result[1] = yMovePos;
-        return result;
     }
 
     private void gameProcess() {
         boolean fff = true;
         while (fff) {
-            this.setMovePos(-1, -1);
             if (FieldAnalyzer.playerCheck(field, player1.getColor())) {
-                if (player1.getClass().equals(HumanPlayer.class)) {
-                    while (FieldAnalyzer.analyze(field, xMovePos, yMovePos, player1.getColor()) == 0) {
-                        fieldGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                            @Override
-                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                                int i = (int) (position / 8);
-                                int j = position % 8;
-                                setMovePos(i, j);
-                                synchronized (gameThread) {
-                                    gameThread.notify();
-                                }
-                            }
-                        });
-                        synchronized (gameThread) {
-                            try {
-                                gameThread.wait();
-                            } catch (InterruptedException ex) {
-                                ex.printStackTrace();
-                            }
-                        }
-                        //Убрать onItemClickListener
-                        fieldGrid.setOnItemClickListener(null);
-                    }
-                }
                 player1.makeMove(field);
+                refreshFieldGrid();
             }
-            revAdapter.notifyDataSetChanged();
 
 
-//            try{
-//                Thread.sleep(1000);
-//            } catch(InterruptedException ex){
-//                Thread.currentThread().interrupt();
-//            }
-
-            this.setMovePos(-1, -1);
             if (FieldAnalyzer.playerCheck(field, player2.getColor())) {
-                if (player2.getClass().equals(HumanPlayer.class)) {
-                    while (FieldAnalyzer.analyze(field, xMovePos, yMovePos, player2.getColor()) == 0) {
-                        fieldGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                            @Override
-                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                                int i = (int) (position / 8);
-                                int j = position % 8;
-                                setMovePos(i, j);
-                                synchronized (gameThread) {
-                                    gameThread.notify();
-                                }
-                            }
-                        });
-                        synchronized (gameThread) {
-                            try {
-                                gameThread.wait();
-                            } catch (InterruptedException ex) {
-                                ex.printStackTrace();
-                            }
-                        }
-                        //Убрать onItemClickListener
-                        fieldGrid.setOnItemClickListener(null);
-                    }
-                }
                 player2.makeMove(field);
+                refreshFieldGrid();
             }
 
             if (FieldAnalyzer.gameOver(field)) {
                 break;
             }
         }
+    }
+
+    private void refreshFieldGrid(){
+        fieldGrid.post(new Runnable() {
+            @Override
+            public void run() {
+                revAdapter.makeFieldVect(field);
+                revAdapter.notifyDataSetChanged();
+            }
+        });
     }
 }
